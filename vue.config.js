3b86514af36925270284e216d14ef51978404e88
helihuo <leizi840228>
2020-03-09 02:10:33 +0800

    打包配置修改

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
 // const path = require('pat
 module.exports = {
+  publicPath:'/minemap-tools/',
   pages: {
     index: {
       entry: 'demo/main.js',
